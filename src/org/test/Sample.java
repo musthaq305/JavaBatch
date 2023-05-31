60d265bcd01a3331153c57b54babc981e588213c
musthaq305 <musthaqmohamed305@gmail.com>
2023-05-31 15:17:34 +0530

    Bala added his Code

--- src/org/test/Sample.java
+++ src/org/test/Sample.java
@@ -2,6 +2,9 @@ package org.test;
 
 public class Sample {
 
+	private void m2() {
+    System.out.println("Bala Complete his work");
+	}
 	public static void main(String[] args) {
 		
 		System.out.println("Data");
